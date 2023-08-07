c025a725981a4ee18671c7cef6ed5352bbb71efa
Mehrad Hamzeloo <Mehradh79@gmail.com>
2023-08-07 13:03:57 +0330

    Project docker-compose File Added and Project can be ran on Docker

--- src/redis-config.js
+++ src/redis-config.js
@@ -2,10 +2,10 @@ const Redis = require('ioredis');
 
 // Replace the following connection details with your Redis server configuration
 const redisOptions = {
-  host: 'localhost', // Replace with your Redis server host
-  port: 6379, // Replace with your Redis server port
+  host: 'redis', // Replace with your Redis server host
+  port: 6379 // Replace with your Redis server port
 };
 
+// const redisClient = new Redis(redisOptions);
 const redisClient = new Redis(redisOptions);
-
 module.exports = redisClient;
