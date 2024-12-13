d1bfc18b9ec28f5c6b74a71d70131fdc68a9c1a3
mehrad <mehradh79@gmail.com>
2024-12-13 15:07:48 +0330

    [modify]token expiration time.

--- src/services/Token/token.service.ts
+++ src/services/Token/token.service.ts
@@ -3,5 +3,5 @@ import jwt from 'jsonwebtoken';
 export default function createToken(user) {
     const secretKey = process.env.ACCESS_TOKEN_SECRET ?? 'Access_Token_Secret'
 
-    return jwt.sign({userId: user.id}, secretKey, {expiresIn: '60m'})
+    return jwt.sign({userId: user.id}, secretKey, {expiresIn: '120m'})
 }
