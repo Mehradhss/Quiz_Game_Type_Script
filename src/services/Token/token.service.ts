5aa5476919c49401682ad39861df89081665669b
mehrad <mehradh79@gmail.com>
2024-12-14 17:13:10 +0330

    [fix]create token expiration time.

--- src/services/Token/token.service.ts
+++ src/services/Token/token.service.ts
@@ -1,14 +1,18 @@
 import jwt from 'jsonwebtoken';
 
 export default function createToken(userId, type) {
-    let secretKey
+    let secretKey;
+    let expireTime;
+
     switch (type) {
         case 'refresh':
             secretKey = process.env.REFRESH_TOKEN_SECRET ?? 'Refresh_Token_Secret'
+            expireTime = '7d'
             break
 
         default:
             secretKey = process.env.ACCESS_TOKEN_SECRET ?? 'Access_Token_Secret'
+            expireTime = '120m'
             break
     }
 
