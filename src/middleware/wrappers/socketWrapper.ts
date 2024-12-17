7bfce93afb331ac00c40aef293d4742c4e151101
mehrad <mehradh79@gmail.com>
2024-12-17 11:08:16 +0330

    [refactor]new socket wrapping system and handling socket exceptions.

--- src/middleware/wrappers/socketWrapper.ts
+++ src/middleware/wrappers/socketWrapper.ts
@@ -1,15 +1,20 @@
 import {Socket} from 'socket.io'
 
-export default function socketWrapper(socket: Socket, eventName: string, handler: (...args: any[]) => Promise<void>) {
+export default function socketWrapper(socket: Socket, eventName: string, handler: (...args: any[]) => Promise<void>, exceptionEventName?: string) {
     socket.on(eventName, async (...args: any[]) => {
         try {
             await handler(...args);
         } catch (error: any) {
-            socket.emit('exception', {
-                'data' : {
-                    message : "this is a simple error message from socket wrapper"
+            const exceptionName = exceptionEventName ?? "exception"
+
+            const dataToEmit = {
+                error: {
+                    message: `an ${exceptionName} accord: ${error.message}}`
                 }
-            } )
+            };
+
+            socket.emit(exceptionName, dataToEmit)
+
             console.error('an error occurred in socket event handler:', error)
         }
     })
