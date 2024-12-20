6030b679afcaa46bf38d641ba63d1b38554075d1
mehrad <mehradh79@gmail.com>
2024-12-20 16:28:28 +0330

    [refactor]minor changes in category create.

--- src/controllers/v1/Category/category.controller.ts
+++ src/controllers/v1/Category/category.controller.ts
@@ -27,54 +27,54 @@ export class CategoryController {
         }
     })
 
-    create = async (req: expressJwt.Request, res: express.Response) => {
-        try {
-            if (!req.auth?.isAdmin) {
-                return res.status(401).json({
-                    data: {
-                        message: "not authorized"
-                    }
-                })
-            }
-
-            const body = {...req.body}
-
-            if (!body?.title) {
-                return res.status(422).json({
-                    data: {
-                        message: "category title is required"
-                    }
-                })
-            }
-
-            const categoryRepository = dataSource.getRepository(Category);
-
-            const categoryExists = await categoryRepository.findOne({
-                where: {
-                    title: body?.title
+    create = asyncHandler(async (req: expressJwt.Request, res: express.Response) => {
+            try {
+                if (!req.auth?.isAdmin) {
+                    res.status(401).json({
+                        data: {
+                            message: "not authorized"
+                        }
+                    })
                 }
-            })
 
-            if (categoryExists) {
-                res.status(422).json({
-                    message: "category already exists!"
+                const body = {...req.body}
+
+                if (!body?.title) {
+                    res.status(422).json({
+                        data: {
+                            message: "category title is required"
+                        }
+                    })
+                }
+
+                const categoryRepository = dataSource.getRepository(Category);
+
+                const categoryExists = await categoryRepository.findOne({
+                    where: {
+                        title: body?.title
+                    }
                 })
+
+                if (categoryExists) {
+                    res.status(422).json({
+                        message: "category already exists!"
+                    })
+                }
+
+                const category = new Category();
+                category.title = body?.title;
+                await categoryRepository.save(category)
+
+                res.status(201).json({
+                    data: {category: category}
+                })
+
+            } catch (e) {
+                res.status(500).json({
+                    message: "there is an server error please try again!"
+                })
+
+                console.log(`category creation error: ${e}`)
             }
-
-            const category = new Category();
-            category.title = body?.title;
-            await categoryRepository.save(category)
-
-            return res.status(201).json({
-                data: {category: category}
-            })
-
-        } catch (e) {
-            res.status(500).json({
-                message: "there is an server error please try again!"
-            })
-
-            console.log(`category creation error: ${e}`)
-        }
-    }
+        })
 }
